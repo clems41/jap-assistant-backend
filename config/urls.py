379bc466cef54de0d68e493483534fa51d772f16
Clément Niot <clement.niot@gmail.com>
2026-05-22 15:44:26 +0200

    refactor(bracket): suppression de l'app bracket

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,6 @@ api_v1_patterns = [
     path("auth/", include("apps.users.urls")),
     path("tournaments/", include("apps.tournaments.urls")),
     path("tournaments/<int:tournament_id>/pairs/", include("apps.players.urls")),
-    path("tournaments/<int:tournament_id>/bracket/", include("apps.brackets.urls")),
 ]
 
 urlpatterns = [
